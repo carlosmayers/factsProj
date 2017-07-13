a4b8d5975e23abbf4336967ce870aac96c233b61
jalensimons33 <jalensimons33@gmail.com>
2017-07-13 11:25:43 -0400

    Add files via upload

--- beatitapp/main.py
+++ beatitapp/main.py
@@ -13,6 +13,42 @@ class MainHandler(webapp2.RequestHandler):
      template = jinja2_environment.get_template('beatit.html')
      self.response.write(template.render())
 
+class FaceShape(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('face_shape.html')
+     self.response.write(template.render())
+
+class EyeShape(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('eye_shape.html')
+     self.response.write(template.render())
+
+class BrowShape(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('brow_shape.html')
+     self.response.write(template.render())
+
+class EyeColor(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('eye_color.html')
+     self.response.write(template.render())
+
+class SkinType(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('skin_type.html')
+     self.response.write(template.render())
+
+class SkinTone(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('skin_tone.html')
+     self.response.write(template.render())
+
+class LipsHandler(webapp2.RequestHandler):
+	def get(self):
+	 template = jinja2_environment.get_template('lips.html')
+     self.response.write(template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
 ], debug=True)
