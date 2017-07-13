5063836c4ec5085ea9db8b25cd861a9b22891415
Bright Zipporah <zipporah.bright@infotech.hamptonu.edu>
2017-07-13 11:44:06 -0400

    created login

--- beatitapp/main.py
+++ beatitapp/main.py
@@ -12,7 +12,20 @@ class MainHandler(webapp2.RequestHandler):
   def get(self):
      template = jinja2_environment.get_template('beatit.html')
      self.response.write(template.render())
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
 
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
 ], debug=True)
