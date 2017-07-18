e17f58d5824af596c773b8a3105a77bdfb93db07
Green Alexa <alexa.green@infotech.hamptonu.edu>
2017-07-18 11:09:56 -0400

    choices pages work

--- beatitapp/main.py
+++ beatitapp/main.py
@@ -1,4 +1,3 @@
-
 import webapp2
 import logging
 import os
@@ -6,7 +5,7 @@ import jinja2
 
 jinja2_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(
-        os.path.dirname(__file__)))
+    os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -15,52 +14,39 @@ class MainHandler(webapp2.RequestHandler):
 
 class FaceShape(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('face_shape.html')
-     self.response.write(template.render())
+        template = jinja2_environment.get_template('face_shape.html')
+        self.response.write(template.render())
 
 class EyeShape(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('eye_shape.html')
-     self.response.write(template.render())
+        template = jinja2_environment.get_template('eye_shape.html')
+        self.response.write(template.render())
 
 class BrowShape(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('brow_shape.html')
-     self.response.write(template.render())
+        template = jinja2_environment.get_template('brow_shape.html')
+        self.response.write(template.render())
 
 class EyeColor(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('eye_color.html')
-     self.response.write(template.render())
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
-
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
+        template = jinja2_environment.get_template('eye_color.html')
+        self.response.write(template.render())
 
 class SkinType(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('skin_type.html')
-     self.response.write(template.render())
+        template = jinja2_environment.get_template('skin_type.html')
+        self.response.write(template.render())
 
 class SkinTone(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('skin_tone.html')
-     self.response.write(template.render())
+        template = jinja2_environment.get_template('skin_tone.html')
+        self.response.write(template.render())
 
 class LipsHandler(webapp2.RequestHandler):
     def get(self):
-     template = jinja2_environment.get_template('lips.html')
-     self.response.write(template.render())
+        template = jinja2_environment.get_template('lips.html')
+        self.response.write(template.render())
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
